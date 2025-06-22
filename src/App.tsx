1b55f5f949681049bea84687f632ff922c211241
Aradiel <25931501+Arad1el@users.noreply.github.com>
2025-06-22 20:43:07 +0100

    Switched to Vite, updated to lastest version of Simple-as-pie

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { PieChart, DataSet } from 'simple-as-pie';
+import { PieChart, type DataSet } from 'simple-as-pie';
+import Alert from '@mui/material/Alert';
+import { Button, Card, CardActions, CardContent, Container, TextField, Grid, CardHeader } from '@mui/material';
+
 
 const colourBits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 
@@ -14,26 +17,6 @@ const generateRandomColour = () => {
   return output;
 }
 
-const clickedFunction = (event: React.PointerEvent, data: DataSet) => {
-    alert(`You just clicked the segment with the colour ${data.colour}`);
-}
-
-const demodata: DataSet[] = [
-  {
-    value: Math.round(Math.random() * 100),
-    colour: generateRandomColour(),  
-    events: {
-      onPointerUp: clickedFunction
-    }
-  },
-  {
-    value: Math.round(Math.random() * 100),
-    colour: generateRandomColour(),
-    events: {
-      onPointerUp: clickedFunction
-    }
-  }
-];
 
 const demoProps = {
   width: 300,
@@ -44,9 +27,36 @@ const demoProps = {
 }
 
 const App = () => {
-  const [data, setData] = useState(demodata);
   const [svgProps, setSvgProps] = useState(demoProps);
- 
+  const [alert, setAlert] = useState<null | string>(null);
+
+  const clickedFunction = (event: React.PointerEvent, data: DataSet) => {
+    console.log("Segment clicked", event.currentTarget);
+    setAlert(`You just clicked the segment with colour ${data.colour}`);
+  }
+
+  const closeAlert = () => {
+    setAlert(null);
+  }
+
+  const demodata: DataSet[] = [
+    {
+      value: Math.round(Math.random() * 100),
+      colour: generateRandomColour(),  
+      events: {
+        onPointerUp: clickedFunction
+      }
+    },
+    {
+      value: Math.round(Math.random() * 100),
+      colour: generateRandomColour(),
+      events: {
+        onPointerUp: clickedFunction
+      }
+    }
+  ]; 
+  const [data, setData] = useState(demodata);
+
   const addMoreData = () => {
     const newEntry: DataSet = {
       value: Math.round(Math.random() * 100),
@@ -99,64 +109,127 @@ const App = () => {
   }
 
   return (
-    <div className="app-container">
-      <h1>Simple-as-pie Demo</h1>      
-      
-      <div>
-        <p>This was built using <a href="https://github.com/Arad1el/simple-as-pie/tree/v2.2.3">Simple-As-Pie v2.2.3</a></p>
-        <p><a href="https://github.com/Arad1el/simple-as-pie-demo-page">View the source code for this page</a></p>
-        <p>Feel free to play around with the parameters for the segments, or adding and deleting them.</p>
-        <p>Pointer Up events are now included, so click on a segment to see that in action</p>
-      </div>
-
-      <div className="svg-props">
-        <h2>Svg properties</h2>
-        <div className="svg-prop-inputs">
-          <label>Width:</label>
-          <input type="number" name="width" value={svgProps.width} onChange={handleSvgChange} />
-          <label>Height:</label>
-          <input type="number" name="height" value={svgProps.height} onChange={handleSvgChange} />
-        </div>
-      </div>
-
-      <div className="data-section">
-        <h2>DataSet entries</h2>
-        <div className="data-entries">
-          {data.map(({value, colour}, index) => (
-            <div key={index} className="data-entry">
-              <div className="left">
-              <label>Value:</label><input type="number" value={value} name="value" onChange={(event) => handleValueChange(event.target.value, index)} />
-              
-              <label>Colour:</label><input type="text" value={colour} name="colour" onChange={(event) => handleColourChange(event.target.value, index)} />
-                  </div>
-                  <div className="right">
-                    <button className="delete" onClick={() => deleteEntry(index)}>Delete</button>
-                  </div>
-            </div>
-          ))}
-        </div>
-        <button className="add-more" onClick={addMoreData}>Add more data</button>
-      </div>
-
-      <div className="chart-props">
-        <h2>PieChart component properties</h2>
-        <div className="chart-prop-inputs">
-          <label>X:</label>
-          <input type="number" name="x" value={svgProps.x} onChange={handleSvgChange} />
-          <label>Y:</label>
-          <input type="number" name="y" value={svgProps.y} onChange={handleSvgChange} />
-          <label>Radius:</label>
-          <input type="number" name="radius" value={svgProps.radius} onChange={handleSvgChange} />
-        </div>
-      </div>
-      
-      <div className="svg-output">
-        <h2>Output</h2>
-        <svg width={svgProps.width + "px"} height={svgProps.height + "px"}>
-          <PieChart x={svgProps.x} y={svgProps.y} data={data} radius={svgProps.radius} />
-        </svg>
-      </div>
-  </div>
+    <main className="app-container">
+      <Grid container padding="20px" spacing={4}>
+        <Container maxWidth="md">
+          <Grid container columns={1}>
+            <Grid display="flex" justifyContent="center" alignItems="center" size="grow" flexShrink={0} flexBasis="100%">
+              <h1>Simple-as-pie Demo</h1>
+            </Grid>          
+            <Grid>
+              <p>This was built using <a href="https://github.com/Arad1el/simple-as-pie/tree/v2.2.4">Simple-As-Pie v2.2.4</a></p>
+              <p><a href="https://github.com/Arad1el/simple-as-pie-demo-page">View the source code for this page</a></p>
+              <p>Feel free to play around with the parameters for the segments, or adding and deleting them.</p>
+              <p>Pointer Up events are now included, so click on a segment to see that in action</p>
+            </Grid>
+          </Grid>
+        </Container>   
+        
+        <Container maxWidth="lg">
+          <Grid container spacing={4} padding="20px">
+            <Grid container spacing={4} size={12}>
+              {data.map((dataEntry, index) => (
+                <Grid key={index} spacing={4} size={6}>
+                  <Card className="data-entry">
+                    <CardContent className='data-entry-fields'>
+                      <TextField
+                        className='data-entry-field'
+                        required
+                        label="Value"
+                        type="number"
+                        onChange={(event) => handleValueChange(event.target.value, index)}
+                        value={dataEntry.value}
+                      />
+                      <TextField
+                        className='data-entry-field'
+                        required
+                        label="Colour"
+                        type="text"
+                        onChange={(event) => handleColourChange(event.target.value, index)}
+                        value={dataEntry.colour}
+                      />
+                    </CardContent>
+                    <CardActions className='data-entry-actions'>
+                      <Button variant="contained" className="delete" onClick={() => deleteEntry(index)}>Delete</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+            <Grid size={12}>
+              <Button variant="contained" className="add-more" onClick={addMoreData}>Add more data</Button>
+            </Grid>
+            <Grid container spacing={4}>
+              <Card>
+                <CardHeader title="Output" />
+                <CardContent>
+                  <Grid container spacing={4}>
+                    <Grid className='output-controls' size={{xs: 12, sm: 4, md: 3}}>
+                      <Container>
+                        <TextField
+                          required
+                          label="SVG Width"
+                          name="width"
+                          type="number"
+                          onChange={handleSvgChange}
+                          value={svgProps.width}
+                        />
+                        <TextField
+                          required
+                          label="SVG Height"
+                          name="height"
+                          type="number"
+                          onChange={handleSvgChange}
+                          value={svgProps.height}
+                        />
+                        <TextField
+                          required
+                          label="Pie Chart X"
+                          name="x"
+                          type="number"
+                          onChange={handleSvgChange}
+                          value={svgProps.x}
+                        />
+                        <TextField
+                          required
+                          label="Pie Chart Y"
+                          name="y"
+                          type="number"
+                          onChange={handleSvgChange}
+                          value={svgProps.y}
+                        />
+                        <TextField
+                          required
+                          label="Pie Chart Radius"
+                          name="radius"
+                          type="number"
+                          onChange={handleSvgChange}
+                          value={svgProps.radius}
+                        />
+                      </Container>
+                    </Grid>
+                    <Grid className='output-visual' size="grow">
+                      <Grid size={12} minHeight={{xs:70, md: 50}}>
+                      {alert ? (
+                        <Alert severity='info' onClose={closeAlert}>
+                        {alert}
+                      </Alert>
+                      ) : null}
+                      </Grid>
+                      <Grid display="flex" justifyContent="center" alignItems="center" size="grow">
+                        <svg className='output-svg' width={svgProps.width + "px"} height={svgProps.height + "px"}>
+                          <PieChart x={svgProps.x} y={svgProps.y} data={data} radius={svgProps.radius} />
+                        </svg>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grid>
+  </main>
   );
 }
 
